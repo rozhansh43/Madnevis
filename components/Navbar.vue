ac2bdcb06ffc4afef9243074241f06d50ff50513
rozhan <rozhansheikholmoluki@gmail.com>
2021-09-09 01:03:35 +0430

    files added

---
+++ components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <b-navbar type="dark" variant="dark">
+            <b-navbar-nav>
+                <b-nav-item href="#">
+                    خانه
+                </b-nav-item>
+                
+                <b-nav-item href="#">
+                    وبلاگ
+                </b-nav-item>
+
+                <b-nav-item href="#">
+                    ثبت سفارش
+                </b-nav-item>
+
+                <b-nav-item href="#">
+                    تعرفه
+                </b-nav-item>
+
+                <b-nav-item href="#">
+                    خدمات
+                </b-nav-item>
+            </b-navbar-nav>
+        </b-navbar>
+    </div>
+</template>
