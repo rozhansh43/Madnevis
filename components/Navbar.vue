1a99938725602590d1bc6b032a99e4c4d985afa4
rozhan <rozhansheikholmoluki@gmail.com>
2021-09-09 15:35:11 +0430

    50% elements added

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,27 +1,44 @@
 <template>
-    <div>
-        <b-navbar type="dark" variant="dark">
-            <b-navbar-nav>
-                <b-nav-item href="#">
-                    خانه
-                </b-nav-item>
-                
-                <b-nav-item href="#">
-                    وبلاگ
-                </b-nav-item>
+    <b-container class="my-5">
+        <b-row>
+            <b-navbar type="light" variant="light">
+                <b-navbar-nav>
+                    <b-navbar-brand href="#">
+                        <img src="@/src/assets/logo-07.jpg" width="50" class="d-inline-block align-top">
+                    </b-navbar-brand>
 
-                <b-nav-item href="#">
-                    ثبت سفارش
-                </b-nav-item>
+                    <b-nav-item href="#">
+                        خانه
+                    </b-nav-item>
+                    
+                    <b-nav-item href="#">
+                        وبلاگ
+                    </b-nav-item>
 
-                <b-nav-item href="#">
-                    تعرفه
-                </b-nav-item>
+                    <b-nav-item href="#">
+                        ثبت سفارش
+                    </b-nav-item>
 
-                <b-nav-item href="#">
-                    خدمات
-                </b-nav-item>
-            </b-navbar-nav>
-        </b-navbar>
-    </div>
+                    <b-nav-item href="#">
+                        تعرفه
+                    </b-nav-item>
+
+                    <b-nav-item href="#">
+                        خدمات
+                    </b-nav-item>
+                </b-navbar-nav>
+            </b-navbar>
+        </b-row>
+    </b-container>
 </template>
+
+<script>
+
+</script>
+
+<style scoped>
+.container {
+    box-shadow: 2px 2px 2px lightgrey;
+    border-radius: 50px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,105 @@
 <template>
-  <Navbar/>
+  <div>  
+    <Navbar/>
+    <b-container>
+      <b-row>
+        <b-col>
+          <h1>
+            مدنویس چی هست؟
+          </h1>
+
+          <p>
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+          </p>
+
+          <div>
+            <b-button variant="warning">
+              Warning
+            </b-button>
+
+            <b-button variant="light">
+              Light
+            </b-button>
+
+          </div>
+        </b-col>
+
+        <b-col>
+          <b-img src="@/src/assets/sizemini_113.jpg" width="600">
+          </b-img>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container fluid class="blue-section">
+      <b-row>
+        <h2>
+          خدمات مدنویس
+        </h2>
+        
+        <p>
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است
+        </p>
+      </b-row>
+    </b-container>
+
+    <b-container>
+      <b-row>
+        <b-col>
+          <b-img src="@/src/assets/calendar.svg" width="60"/>
+          <h5>
+            تایپ مقالات پزشکی
+          </h5>
+        </b-col>
+
+        <b-col>
+          <b-img src="@/src/assets/folder.svg" width="60"/>
+          <h5>
+            ترجمه متون پزشکی
+          </h5>
+        </b-col>
+
+        <b-col>
+          <b-img src="@/src/assets/file (1).svg" width="60"/>
+          <h5>
+            کتابخانه
+          </h5>
+        </b-col>
+
+        <b-col>
+          <b-img src="@/src/assets/file.svg" width="60"/>
+          <h5>
+            ویراستاری کتاب
+          </h5>
+        </b-col>
+
+        <b-col>
+          <b-img src="@/src/assets/calendar.svg" width="60"/>
+          <h5>
+            سوالات متداول
+          </h5>
+        </b-col>
+
+        <b-col>
+          <b-img src="@/src/assets/calendar (1).svg" width="60"/>
+          <h5>
+            ویراستاری کتاب
+          </h5>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style>
+body{
+  direction: rtl;
+}
+.blue-section {
+  background-color: #0953f0;
+}
+</style>
