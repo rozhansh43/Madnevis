84ed185e8cccf7e06d2d5611d0721d0a6f300555
rozhan <rozhansheikholmoluki@gmail.com>
2021-09-11 05:37:05 +0430

    register page added

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,37 +1,68 @@
 <template>
     <b-navbar type="light">
-        <b-container class="my-5 py-2">
+        <b-container class="my-5 py-2 bg-secondary">
             <b-col cols="8">
                 <b-navbar-nav>
-                    <b-navbar-brand href="#">
-                        <img src="@/src/assets/logo-07.jpg" width="40" class="d-inline-block align-top">
+                    <b-navbar-brand class="d-flex justify-content-center align-items-center mx-3">
+                        <NuxtLink to="/">
+                            <b>
+                                LOGO
+                            </b>
+                        </NuxtLink>
                     </b-navbar-brand>
 
-                    <b-nav-item href="#">
-                        خانه
+                    <b-nav-item>
+                        <NuxtLink to="/my-items" class="nav-link">
+                            خانه
+                            <span class="sr-only">
+                                (current)
+                            </span>
+                          </NuxtLink>
                     </b-nav-item>
                     
-                    <b-nav-item href="#">
-                        وبلاگ
+                    <b-nav-item>
+                        <NuxtLink to="/my-items" class="nav-link">
+                            وبلاگ
+                            <span class="sr-only">
+                                (current)
+                            </span>
+                          </NuxtLink>
                     </b-nav-item>
 
-                    <b-nav-item href="#">
-                        ثبت سفارش
+                    <b-nav-item>
+                        <NuxtLink to="/my-items" class="nav-link">
+                            ثبت سفارش   
+                            <span class="sr-only">
+                                (current)
+                            </span>
+                        </NuxtLink>
                     </b-nav-item>
 
-                    <b-nav-item href="#">
-                        تعرفه
+                    <b-nav-item>
+                        <NuxtLink to="/my-items" class="nav-link">
+                             تعرفه
+                            <span class="sr-only">
+                                (current)
+                            </span>
+                        </NuxtLink>
                     </b-nav-item>
 
-                    <b-nav-item href="#">
-                        خدمات
+                    <b-nav-item>
+                        <NuxtLink to="/my-items" class="nav-link">
+                            خدمات
+                            <span class="sr-only">
+                                (current)
+                            </span>
+                        </NuxtLink>
                     </b-nav-item>
                 </b-navbar-nav>
             </b-col>
 
             <b-col cols="4">
-                <b-button pill variant="success">
-                    ورود/عضویت
+                <b-button pill variant="success" class="border-50">
+                    <NuxtLink to="/register" class="text-white">
+                        ورود/عضویت
+                    </NuxtLink>
                 </b-button>
             </b-col>
         </b-container>
@@ -46,5 +77,14 @@
 .container {
     box-shadow: 2px 2px 2px lightgrey;
     border-radius: 50px;
+}
+
+.nav-link {
+    font-weight: 700;
+}
+
+.btn {
+    text-decoration: none;
+    border-radius: 50px!important;
 }
 </style>
---
+++ pages/Register.vue
@@ -0,0 +1,143 @@
+<template>
+    <b-container class="py-5">
+        <b-row class="mb-4">
+            <div class="col-lg-8 mx-auto text-center">
+            <h1 class="display-4">
+                در مدنویس عضو شوید
+            </h1>
+            <p class="lead mb-0">
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است 
+            </p>
+
+            <p class="lead">
+                با
+                <a href="https://google.com">
+                    شرایط و ضوابط    
+                </a>
+                موافقم
+            </p>
+            </div>
+        </b-row>
+
+        <b-row>
+            <b-col lg="7" class="mx-auto">
+                <div class="bg-white rounded-lg shadow-sm p-5">
+                    <!-- Credit card form tabs -->
+                    <ul role="tablist" class="nav bg-light nav-pills rounded-pill nav-fill mb-3">
+                    <li class="nav-item">
+                        <a data-toggle="pill" href="#nav-tab-card" class="nav-link active rounded-pill">
+                            <i class="fa fa-credit-card"></i>
+                            ثبت نام
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a data-toggle="pill" href="#nav-tab-paypal" class="nav-link rounded-pill">
+                            <i class="fa fa-paypal"></i>
+                            ورود
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a data-toggle="pill" href="#nav-tab-bank" class="nav-link rounded-pill">
+                            <i class="fa fa-university"></i>
+                            مهمان
+                        </a>
+                    </li>
+                    </ul>
+                    <!-- End -->
+
+                    <!-- Credit card form content -->
+                    <div class="tab-content">
+
+                    <!-- credit card info-->
+                    <div id="nav-tab-card" class="tab-pane fade show active">
+                        <p class="alert alert-success">
+                            ثبت نام سما با موفقیت انجام شد
+                        </p>
+                        <form role="form">
+                        <div class="form-group">
+                            <label for="username">
+                                نام و نام خانوادگی
+                            </label>
+                            <input type="text" name="username" placeholder="علی نادری" required class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="cardNumber">
+                                شماره تماس
+                            </label>
+                            <div class="input-group">
+                            <input type="text" name="cardNumber" placeholder="09*****987" class="form-control" required>
+                            <div class="input-group-append">
+                                <span class="input-group-text text-muted">
+                                    <i class="fa fa-cc-visa mx-1"></i>
+                                    <i class="fa fa-cc-amex mx-1"></i>
+                                    <i class="fa fa-cc-mastercard mx-1"></i>
+                                </span>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-8">
+                            <div class="form-group">
+                                <label>
+                                    <span class="hidden-xs">
+                                        تاریخ تولد
+                                    </span>
+                                </label>
+                                <div class="input-group">
+                                <input type="number" placeholder="MM" name="" class="form-control" required>
+                                <input type="number" placeholder="YY" name="" class="form-control" required>
+                                </div>
+                            </div>
+                            </div>
+                            <div class="col-sm-4">
+                            <div class="form-group mb-4">
+                                <label data-toggle="tooltip" title="Three-digits code on the back of your card">CVV
+                                    <i class="fa fa-question-circle"></i>
+                                </label>
+                                <input type="text" required class="form-control">
+                            </div>
+                            </div>
+                        </div>
+                        <button type="button" class="subscribe btn btn-primary btn-block rounded-pill shadow-sm"> Confirm  </button>
+                        </form>
+                    </div>
+                    <!-- End -->
+
+                    <!-- Paypal info -->
+                    <div id="nav-tab-paypal" class="tab-pane fade">
+                        <p>Paypal is easiest way to pay online</p>
+                        <p>
+                        <button type="button" class="btn btn-primary rounded-pill"><i class="fa fa-paypal mr-2"></i> Log into my Paypal</button>
+                        </p>
+                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                    <!-- End -->
+
+                    <!-- bank transfer info -->
+                    <div id="nav-tab-bank" class="tab-pane fade">
+                        <h6>Bank account details</h6>
+                        <dl>
+                        <dt>Bank</dt>
+                        <dd> THE WORLD BANK</dd>
+                        </dl>
+                        <dl>
+                        <dt>Account number</dt>
+                        <dd>7775877975</dd>
+                        </dl>
+                        <dl>
+                        <dt>IBAN</dt>
+                        <dd>CZ7775877975656</dd>
+                        </dl>
+                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                    <!-- End -->
+                    </div>
+                    <!-- End -->
+
+                </div>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
